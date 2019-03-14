bae51bcc7619d581a4fd6fbcd3e36d168a8cf9ed
Samuel Barksdale <samuelbarksdale@Samuels-MacBook-Air.local>
2019-03-14 15:53:32 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var carFour = "king"
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
