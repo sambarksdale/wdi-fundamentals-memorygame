e8ceac56ced91753312fd895ad353db060a2c573
Samuel Barksdale <samuelbarksdale@Samuels-MacBook-Air.local>
2019-03-14 19:19:36 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,20 @@
-console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var carFour = "king"
 
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
+cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("you found a match");
+	} else {
+		alert("sorry try again");
+	}
+} else {
+	alert("pick another card");
+}
